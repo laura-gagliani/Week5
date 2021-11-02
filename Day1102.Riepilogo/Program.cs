d9c10ef4300d61481526486e86715a019f87d6ba
Laura Gagliani <gaglianilaura@gmail.com>
2021-11-02 09:35:43 +0100

    Aggiungere i file di progetto.

---
+++ Day1102.Riepilogo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Day1102.Riepilogo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int age;    //dichiarazione
+            age = 30;   //assegnazione
+            int num = 3; //inizializzazione
+
+            const int c = 3; //costante, non posso assegnare diverso valore
+
+
+        }
+    }
+}
