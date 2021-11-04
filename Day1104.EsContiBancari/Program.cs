fd6bbcb0afd66a18d3316ac42525280b38cfaffe
Laura Gagliani <gaglianilaura@gmail.com>
2021-11-04 09:29:22 +0100

    gio mattina

---
+++ Day1104.EsContiBancari/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day1104.EsContiBancari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
